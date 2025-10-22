78e207f3bff24d8161cbf66892ad61b9bcb7c87e
muchnssimaja <much.n.s.simaja@gmail.com>
2025-10-22 08:20:52 +0700

    mengenal view

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('home/welcome');
-    return 'Hallo Laravel';
+    return view('index');
 });
 
 Route::get('about', function () {
-    return 'Ini adalah halaman <b>about</b>';
+    return view('about');
 });
 
 Route::get('mahasiswa', function () {
-    return 'Ini adalah halaman <b>Mahasiswa</b>';
+    return view('mahasiswa');
 });
 
 Route::get('profile', function () {
     $nama = 'elMucH';
-    return 'Ini adalah halaman <b>profile</b>, nama saya adalah ' . $nama;
+    return view('profile', compact('nama'));
 });
