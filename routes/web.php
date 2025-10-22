e31450f77c0d1ca804698676375fb8499812487b
muchnssimaja <much.n.s.simaja@gmail.com>
2025-10-22 07:57:20 +0700

    pengenalan route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('home/welcome');
+    return 'Hallo Laravel';
+});
+
+Route::get('about', function () {
+    return 'Ini adalah halaman <b>about</b>';
+});
+
+Route::get('mahasiswa', function () {
+    return 'Ini adalah halaman <b>Mahasiswa</b>';
+});
+
+Route::get('profile', function () {
+    $nama = 'elMucH';
+    return 'Ini adalah halaman <b>profile</b>, nama saya adalah ' . $nama;
 });
